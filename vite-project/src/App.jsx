2b2732b7c388e4d2ee4c423c5b19e0cf72338743
Sai_Binayak <sai0binayak99@gmail.com>
2024-10-16 19:44:54 +0530

    2nd Project - RandomColor Generator

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,14 @@
-import Accordian_01 from './Components/Accordian_01/Accordian_01'
-import './App.css'
+import Accordian_01 from "./Components/Accordian_01/Accordian_01";
+import "./App.css";
+import RandomColor_02 from "./Components/RandomColor_02/RandomColor_02";
 
 function App() {
-
   return (
-    <Accordian_01/>
-  )
+    <>
+      <Accordian_01 />
+      <RandomColor_02 />
+    </>
+  );
 }
 
-export default App
+export default App;
