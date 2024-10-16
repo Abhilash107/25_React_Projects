de1bd670d2c6b1ce952569264afd6c1eac1025f5
Sai_Binayak <sai0binayak99@gmail.com>
2024-10-16 19:52:50 +0530

    3rd Project - Star Rating

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,14 @@
 import Accordian_01 from "./Components/Accordian_01/Accordian_01";
 import "./App.css";
 import RandomColor_02 from "./Components/RandomColor_02/RandomColor_02";
+import StarRating_03 from "./Components/StarRating_03/StarRating_03";
 
 function App() {
   return (
     <>
       <Accordian_01 />
       <RandomColor_02 />
+      <StarRating_03 noOfStars={10}/>
     </>
   );
 }
